180e116b5adfbadaeb46ea2d1644019356b758e1
Harrison7318 <Harr7318@fredonia.edu>
2021-11-22 17:26:58 -0500

    Multiple changes
    Updated component
    Made forLoop.h
    got rid of makeCSS() from createHTMLfile.h so added CSS file
    
    TemplateMatcher
    16 int* rulesViolatedLines;
    
    105 if statement body updated with line nums
    
    134 array
    
    156 suggestion string

--- SCA/core/src/Component.h
+++ SCA/core/src/Component.h
@@ -1,6 +1,6 @@
 #ifndef COMPONENT
 #define COMPONENT
-
+//my machine
 #include "SCA.h"
 
 using namespace std;
@@ -8,22 +8,20 @@ using namespace std;
 class Component 
 {
 private:
-	Node* treeRootNode;
-	Node* componentRootNode;
-
 	int statementTypeint;
 	string statementType;
 	Node* startOfStatement;
 	string componentType;
 	vector<Node*> statementInstances;
-
+	vector<int> startLine;
+	int startLineCount;
+	vector<int> endLine;
+	int endLineCount;
 public:
-	// Default Constructor
 	Component();
-	// Contructor with component root node
-	Component(Node* compRtPtr);
-	// Constructor with component root node and tree root node
-	Component(Node* compRtPtr, Node* treeRtPtr);
+
+	Component(int type);
+
 	//Pass in 0 for 'iterationStatement' and 1 for 'selectionStatement'.
 	void setStatementType(int type);
 
@@ -38,21 +36,48 @@ public:
 
 	//returns vector of pointers to all nodes in the statement.
 	vector<Node*> getStatementInstances();
+
+	//goes to the bottom right node start from start node* and sets endLine.
+	void addAnEndLine(Node* start);
+
+	//returns endLine.
+	vector<int> getEndLine();
+
+	//returns amount of endLines.
+	int getEndLineCount();
+
+	//sets startLine as int start.
+	void addAnStartLine(int start);
+
+	//returns startLine.
+	vector<int> getStartLine();
+
+	//returns amount of startLines.
+	int getStartLineCount();
+
+	//searches from start node to find data, returns first node with the same data in it.
+	Node* find(string data, Node* start);
 };
 
-Component::Component() {
-	componentRootNode = nullptr;
-	treeRootNode = nullptr;
+Component::Component()
+{
+	startLineCount = 0;
+	endLineCount = 0;
 }
 
-Component::Component(Node* compRtPtr) {
-	componentRootNode = compRtPtr;
-	treeRootNode = nullptr;
-}
+Component::Component(int type)
+{
+	statementTypeint = type;
 
-Component::Component(Node* compRtPtr, Node* treeRtPtr) {
-	componentRootNode = compRtPtr;
-	treeRootNode = treeRtPtr;
+	if (type == 0)
+		statementType = "iterationStatement";
+	else if (type == 1)
+		statementType = "selectionStatement";
+	else
+		statementType = "Incorrect input for setStatementType()";
+
+	startLineCount = 0;
+	endLineCount = 0;
 }
 
 void Component::setStatementType(int type){
@@ -85,6 +110,57 @@ Node* Component::getStartOfStatement(){
 
 vector<Node*> Component::getStatementInstances() {
 	return statementInstances;
+}
+
+void Component::addAnEndLine(Node* start)
+{
+	Node* nodeIter = start;
+	
+	while (nodeIter->getChildCount() != 0)
+		nodeIter = nodeIter->getChild(nodeIter->getChildCount() - 1);
+
+	endLine[endLineCount] = nodeIter->getLineNum();
+	endLineCount++;
+}
+
+vector<int> Component::getEndLine() {
+	return endLine;
+}
+
+int Component::getEndLineCount() {
+	return endLineCount;
+}
+
+void Component::addAnStartLine(int start) {
+	startLine[startLineCount] = start;
+	startLineCount++;
+}
+
+vector<int> Component::getStartLine() {
+	return startLine;
+}
+
+int Component::getStartLineCount() {
+	return startLineCount;
+}
+
+Node* Component::find(string data, Node* start)
+{
+	Node* nodeIter = start;
+
+	if (nodeIter->getData() == data)
+	{
+		return nodeIter;
+	}
+
+	if (nodeIter->getChildCount() == 0)
+		return nullptr;
+
+	int totalChildren = nodeIter->getChildCount();
+
+	for (int i = 0; i < totalChildren; i++) {
+		find(data, nodeIter->getChild(i));
+	}
 }
 
 #endif // !COMPONENT
--- SCA/core/src/Template_Matcher.h
+++ SCA/core/src/Template_Matcher.h
@@ -9,16 +9,12 @@ using namespace std;
 class Template_Matcher {
 private:
 	unordered_map <int, Rule> template_table;
-	string iterationStmt = "iterationStatement";
-	string selectionStmt = "selectionStatement";
 	int rulesViolatedEntries;
 	int rulesToCheckEntries;
 	int* rulesToCheck;
 	int* rulesViolated;
+	int* rulesViolatedLines;
 	string suggestions;
-	vector<Node*> iterationNodes;
-	vector<Node*> selectionNodes;
-	vector<While_Loop*> whileLoopComponents;
 
 public:
 	//constructor sets entries to 0 and suggestions to "".
@@ -47,19 +43,9 @@ public:
 
 	//returns template table size, the number of rules.
 	int getTemplateTableSize();
-
-	// fills the iteration node vector with all node instances of iteration statements in source code
-	void _fillIterationNodeVector(Tree* tree);
-
-	// fills the selection node vector with all node instances of selection statements in source code
-	void _fillSelectionNodeVector(Tree* tree);
-
-	// Calls the fill node vector functions then iterates through each vector handing node to corresponding component class to build component
-	void retreiveComponents(Tree* tree);
-
 };
 
-Template_Matcher::Template_Matcher() 
+Template_Matcher::Template_Matcher()
 {
 	rulesToCheckEntries = 0;
 	rulesViolatedEntries = 0;
@@ -118,9 +104,22 @@ bool Template_Matcher::checkFullViolation(Node* rt, Rule currRule)
 
 	if (treeRule == templateRule)
 	{
-		rulesViolated[rulesViolatedEntries] = currRule.getRuleNum();
-		rulesViolatedEntries++;
-		return true;
+		bool alreadyThere = false;
+		for (int i = 0; i < template_table.size(); i++)
+		{
+			if ((rulesViolated[i] == currRule.getRuleNum()) && (rulesViolatedLines[i] == rt->getLineNum()))
+				alreadyThere = true;
+
+			if (alreadyThere)
+				break;
+		}
+		if (!alreadyThere)
+		{
+			rulesViolated[rulesViolatedEntries] = currRule.getRuleNum();
+			rulesViolatedLines[rulesViolatedEntries] = rt->getLineNum();
+			rulesViolatedEntries++;
+			return true;
+		}
 	}
 	return false;
 }
@@ -130,8 +129,9 @@ void Template_Matcher::setTemplateTable(unordered_map <int, Rule> templatetable)
 }
 
 void Template_Matcher::setRulesArraySize() {
-	rulesViolated = new int [template_table.size()];
-	rulesToCheck = new int [template_table.size()];
+	rulesViolated = new int[template_table.size()];
+	rulesToCheck = new int[template_table.size()];
+	rulesViolatedLines = new int[template_table.size()];
 	rulesToCheckEntries = 0;
 	rulesViolatedEntries = 0;
 }
@@ -153,76 +153,10 @@ string Template_Matcher::outputSuggestions()
 	{
 		if (rulesViolated[i] == -1)
 			break;
-		suggestions += rulesViolated[i] + ": " + template_table[rulesViolated[i]].getSuggestion() + "\n";
+		suggestions += "Line " + rulesViolatedLines[i];
+		suggestions += ": " + template_table[rulesViolated[i]].getSuggestion() + "\n";
 	}
 
 	return suggestions;
 }
-
-void Template_Matcher::_fillIterationNodeVector(Tree* tree) {
-	tree->filltokenNodeVector(iterationStmt, tree->getRoot());
-	iterationNodes = tree->getTokenNodes();
-}
-
-	
-void Template_Matcher::_fillSelectionNodeVector(Tree* tree) {
-	tree->filltokenNodeVector(selectionStmt, tree->getRoot());
-	selectionNodes = tree->getTokenNodes();
-}
-
-void Template_Matcher::retreiveComponents(Tree* tree) {
-	// populate vectors with nodes from tree
-	_fillIterationNodeVector(tree);
-	_fillSelectionNodeVector(tree);
-	Node* temp;
-
-	// iterate though each node that contains an iterationStatement
-	for (int i = 0; i < iterationNodes.size(); i++) {
-		temp = iterationNodes[i];
-		while (temp->getChildCount() != 0) {
-			temp = temp->getChild(0);
-		}
-		switch(temp->getData()) {
-			case "for":
-				// hand control to for_component, pass in rootNode of Tree and iterationNode[i]
-				break;
-			case "while":
-				While_Loop* aWhileLoop = new While_Loop(iterationNodes[i], tree->getRoot(), "while");
-				aWhileLoop->extractAllInfo();
-				whileLoopComponents.push_back(aWhileLoop);
-				break;
-			case "do":
-				While_Loop* aDoWhileLoop = new While_Loop(iterationNodes[i], tree->getRoot(), "do");
-				aDoWhileLoop->extractAllInfo();
-				whileLoopComponents.push_back(aDoWhileLoop);
-				break;
-			default:
-				break;
-		}
-	}
-
-	// iterate through each node that contains a selectionStatement
-	for (int i = 0; i < selectionNodes.size(); i++) {
-		temp = iterationNodes(i);
-		while (temp->getChildCount() != 0) {
-			temp = temp->getChild(0);
-		}
-		switch(temp->getData()) {
-			case "if":
-				// hand control to if_component, pass in rootNode of Tree and iterationNode[i]
-				break;
-			case "switch":
-				// hand control to switch_component, pass in rootNode of Tree and iterationNode[i]
-				break;
-			default:
-				break;
-		}
-	}
-
-	for (int i = 0; i < whileLoopComponents.size(); i++) {
-		whileLoopComponents[i]->printLoopInfo();
-	}
-}
-
-
 #endif // !TEMPLATE_MATCHER
